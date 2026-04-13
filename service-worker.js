ad897e385970edcb641d873691f5c62d3cf69585
fabriciobenigno <fabriciobenigno803@gmail.com>
2026-04-13 18:28:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,49 @@
 const CACHE_NAME = "palco-pro-cache-v1";
 
 const APP_ASSETS = [
-  "/Palco-Pro/",
-  "/Palco-Pro/index.html",
-  "/Palco-Pro/manifest.json",
-  "/Palco-Pro/palco-pro-icon.png",
-  "/Palco-Pro/palco-pro-share.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./palco-pro-icon.png",
+  "./palco-pro-share.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(APP_ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      );
-    })
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  const request = event.request;
-
-  if (request.method !== "GET") {
-    return;
-  }
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
 
-      return fetch(request)
+      return fetch(event.request)
         .then((networkResponse) => {
-          const responseClone = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(request, responseClone);
-          });
-
+          const clone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
           return networkResponse;
         })
-        .catch(() => {
-          return caches.match("/Palco-Pro/index.html");
-        });
+        .catch(() => caches.match("./index.html"));
     })
   );
 });
